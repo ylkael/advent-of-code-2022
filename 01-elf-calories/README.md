2d61e0a0368d4be13a34227fd9d1b8dc05f11a8b
elinaylk <creep-89@hotmail.com>
2023-01-18 14:28:24 +0200

    elf calories part 2

--- 01-elf-calories/README.md
+++ 01-elf-calories/README.md
@@ -1,5 +1,10 @@
+Part 1:  
 Find the Elf carrying the most Calories.  
 How many total Calories is that Elf carrying?
+
+Part 2:  
+Find the top three Elves carrying the most Calories.  
+How many Calories are those Elves carrying in total?
 
 https://adventofcode.com/2022/day/1
 
--- 01-elf-calories/calories.py
+++ 01-elf-calories/calories.py
@@ -1,11 +1,18 @@
 with open('01-elf-calories/calories.txt') as file:
     calories = file.read()
     elf = calories.split("\n\n")
+    sum_calories = []
     most_calories = 0
     for calories in elf:
         calories = calories.split("\n")
-        total= sum(int(i) for i in calories)
+        total = sum(int(i) for i in calories)
         if total > most_calories:
             most_calories = total
-        
+        sum_calories.append(total)
+    
+    top_3 = sorted(sum_calories, reverse=True)[:3]
+    top_3_sum = sum(top_3)
+
 print('Most calories:',most_calories)
+print('Top 3 calories:', *top_3, sep=' ')
+print('Sum of top 3 calories:', top_3_sum)
