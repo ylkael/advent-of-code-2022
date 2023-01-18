0fa69bfc7f3efef2a50aa007eaaf94c3b22a6f05
elinaylk <creep-89@hotmail.com>
2023-01-18 13:48:19 +0200

    elf calories

---
+++ elf-calories/calories.py
@@ -0,0 +1,11 @@
+with open('elf-calories/calories.txt') as file:
+    calories = file.read()
+    elf = calories.split("\n\n")
+    most_calories = 0
+    for calories in elf:
+        calories = calories.split("\n")
+        total= sum(int(i) for i in calories)
+        if total > most_calories:
+            most_calories = total
+        
+print('Most calories:',most_calories)
