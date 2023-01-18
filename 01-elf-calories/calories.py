48f00b94be47a8081b35ed467267648761527f88
elinaylk <creep-89@hotmail.com>
2023-01-18 14:31:27 +0200

    elf calories part 2 update

--- 01-elf-calories/calories.py
+++ 01-elf-calories/calories.py
@@ -9,10 +9,10 @@ with open('01-elf-calories/calories.txt') as file:
         if total > most_calories:
             most_calories = total
         sum_calories.append(total)
-            
+
     top_3 = sorted(sum_calories, reverse=True)[:3]
     top_3_sum = sum(top_3)
 
-print('Most calories:',most_calories)
+print('Most calories:', most_calories)
 print('Top 3 calories:', *top_3, sep=' ')
 print('Sum of top 3 calories:', top_3_sum)
