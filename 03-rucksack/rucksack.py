2cbf2f3777eeffe5c147f6195d94231a26fa27c1
Elina Ylkänen <ylkael@hotmail.com>
2023-01-20 13:51:51 +0200

    Rucksack Reorganization

--- 03-rucksack/rucksack.py
+++ 03-rucksack/rucksack.py
@@ -1,23 +1,19 @@
-
-# a b c d e f g h i j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
-# 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
-
-# A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
-# 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52
-
 with open("03-rucksack/rucksacks_contents.txt") as file:
     contents = file.read()
     contents = contents.split("\n")
+    priorities_sum = 0
     for rucksack in contents:
         rucksack = rucksack.split("\n")
         rucksack = ' '.join(rucksack)
-        print('rucksack:',rucksack)
         first_compartment = rucksack[:len(rucksack) // 2]
-        print('first half:',first_compartment)
         second_compartment = rucksack[len(rucksack) // 2:]
-        print('second half:',second_compartment)
-
-    # contents = [int(i) for i in contents]
-    # contents.sort()
-    # contents.reverse()
-    # print(contents)
+        match_items = []        
+        for item in first_compartment:
+            if item in second_compartment:
+                match_items.append(item)
+        for item in set(match_items):
+            if item.isupper():
+                priorities_sum += (ord(item) - 38)
+            else:
+                priorities_sum += (ord(item) - 96)
+    print('Priorities sum:',priorities_sum)
