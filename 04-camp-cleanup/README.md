30092f720da8b25fb6de9e6c24521fcf67ac5a3a
Elina Ylkänen <ylkael@hotmail.com>
2023-01-25 13:40:29 +0200

    camp cleanup part 1

---
+++ 04-camp-cleanup/README.md
@@ -0,0 +1,20 @@
+Day 4: Camp Cleanup  
+
+Every section has a unique ID number, and each Elf is assigned a range of section IDs.  
+For example, consider the following list of section assignment pairs:  
+
+```
+2-4,6-8  
+2-3,4-5  
+5-7,7-9  
+2-8,3-7  
+6-6,4-6  
+2-6,4-8  
+```
+Some of the pairs have noticed that one of their assignments fully contains the other.  
+For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. 
+
+Part 1:  
+In how many assignment pairs does one range fully contain the other?
+
+https://adventofcode.com/2022/day/4
--- 04-camp-cleanup/camp-cleanup.py
+++ 04-camp-cleanup/camp-cleanup.py
@@ -1,49 +1,28 @@
 with open("04-camp-cleanup/assignment-pairs.txt") as file:
     content = file.read()
     contents = content.split("\n")
-    print(contents)
-    
+    assignments_overlap = 0    
     for pair in contents:
         pair = pair.split("\n")
-        print('pair',pair)
         tasks = pair[0].split(",")
-
-        for i in range(len(pair)):            
+        for i in range(len(pair)):
+            # Elf 1 tasks range            
             elf1_tasks = tasks[i].split("-")
             for i in range(len(elf1_tasks)):
                 elf1_tasks[i] = int(elf1_tasks[i])
-            tasks1_range = []
+            elf1_tasks_range = []
             for i in range(elf1_tasks[0], elf1_tasks[1] + 1):
-                tasks1_range.append(i)
-            print('range1',elf1_tasks,':',tasks1_range)
-
+                elf1_tasks_range.append(i)
+            # Elf 2 tasks range
             elf2_tasks = tasks[1].split("-")
             for i in range(len(elf2_tasks)):
                 elf2_tasks[i] = int(elf2_tasks[i])
-            tasks2_range = []
+            elf2_tasks_range = []
             for i in range(elf2_tasks[0], elf2_tasks[1] + 1):
-                tasks2_range.append(i)
-            print('range2',elf2_tasks,':',tasks2_range)
-
-
-    
-        # print('tasks',tasks)
-
-        # task1 = tasks[0].split("-")
-        # task2 = tasks[1].split("-")
-        # print(task1)
-        # print(task2)
-
-        # for i in range(len(task1)):
-        #     task1[i] = int(task1[i])
-        # for i in range(task1[0], task1[1] + 1):
-        #     print('tasks1:',i)
-        # for i in range(len(task2)):
-        #     task2[i] = int(task2[i])
-        # for i in range(task2[0], task2[1] + 1):
-        #     print('tasks2',i)
-        # print(task1)
-        # print(task2)
-
-
-
+                elf2_tasks_range.append(i)
+            # Check if tasks overlap
+            if set(elf1_tasks_range).issubset(elf2_tasks_range) or set(elf2_tasks_range).issubset(elf1_tasks_range):
+                assignments_overlap += 1
+        
+    print('Total assignment overlaps:',assignments_overlap)
+                
